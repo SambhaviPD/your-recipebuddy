6989358da33155620f9d226db7d7889e86e7bd01
Sambhavi PD <sambhavi@golittlebig.com>
2023-03-26 13:33:24 +0530

    added skeleton code for backend

---
+++ backend/src/main.py
@@ -0,0 +1,49 @@
+from enum import Enum
+
+from fastapi import FastAPI, UploadFile
+
+app = FastAPI(title="Recipe Buddy")
+
+
+@app.get("/")
+async def home():
+    return "Welcome to Recipe Buddy!"
+
+
+@app.get("/recipes/surpriseme/")
+async def get_recipes_random():
+    pass
+
+
+class Cuisine(str, Enum):
+    chinese = "chinese"
+    continental = "continental"
+    indian = "indian"
+    sushi = "sushi"
+    thai = "thai"
+
+
+@app.get("/recipes/{cuisine}")
+async def get_recipes_by_cuisine(cuisine: Cuisine):
+    pass
+
+
+@app.get("/recipes/{ingredients}")
+async def get_recipes_by_ingredients(ingredients: list[str]):
+    pass
+
+
+@app.get("/recipes/{mealcourse}")
+async def get_recipes_by_mealcourse(mealcourse: str):
+    pass
+
+
+@app.get("/recipes/{image}")
+async def get_recipes_by_image(image: UploadFile):
+    pass
+
+
+@app.get("/recipes/{images}")
+async def get_recipe_by_images(images: UploadFile):
+    pass
+
