2532a6f8bc706d9c87883842e944ecf5b4571ef3
Sambhavi PD <sambhavi@golittlebig.com>
2023-03-29 12:10:18 +0530

    added empty streamlit pages, one for each get method

---
+++ frontend/src/👩‍🍳_Recipe_Buddy.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+import pandas as pd
+import numpy as np
+
+st.markdown("## Recipe Buddy, a super easy app for your hunger pangs 😋")
+
+st.markdown("#### Use it to cook the food that you feel like eating right now 🍴")
+
+st.markdown("Upload a picture of your ingreients, and let the app suggest you options 📷")
+
+st.markdown("Or, search by Meal course or Ingredient list. Or, Just ask the app to surprise you 😲")
+
+st.markdown("#### Just click on a menu option on the left 🙌")
