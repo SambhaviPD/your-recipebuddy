dfc2ffe27792765a06fc6c64434de77dcdd558d2
Sambhavi PD <sambhavi@golittlebig.com>
2023-03-31 14:39:45 +0530

    fixed a typo in the main page

--- frontend/src/👩‍🍳_Recipe_Buddy.py
+++ frontend/src/👩‍🍳_Recipe_Buddy.py
@@ -7,7 +7,7 @@ st.markdown("## Recipe Buddy, a super easy app for your hunger pangs 😋")
 
 st.markdown("#### Use it to cook the food that you feel like eating right now 🍴")
 
-st.markdown("Upload a picture of your ingreients, and let the app suggest you options 📷")
+st.markdown("Upload a picture of your ingredients, and let the app suggest you options 📷")
 
 st.markdown("Or, search by Meal course or Ingredient list. Or, Just ask the app to surprise you 😲")
 
