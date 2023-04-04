ebce283cc753c3aad6327497603f1f8db0132fb6
Sambhavi PD <sambhavi@golittlebig.com>
2023-04-04 16:39:34 +0530

    WIP random recipe backend Created environment variable and a method where the Spoonacular API is contructed. For some reason, the env variables are not being picked up. Opening a PR to discuss the same.

--- frontend/src/Recipe_Buddy.py
+++ frontend/src/Recipe_Buddy.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-import pandas as pd
-import numpy as np
+st.set_page_config(
+    page_title="Recipe Buddy",
+    page_icon="👩‍🍳"
+)
 
 st.markdown("## Recipe Buddy, a super easy app for your hunger pangs 😋")
 
