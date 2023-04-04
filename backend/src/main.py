a939abceb2ba0e8d0c9c5271ea56e76a40b04e2e
Sambhavi PD <sambhavi@golittlebig.com>
2023-04-04 16:39:34 +0530

    WIP random recipe backend Created environment variable and a method where the Spoonacular API is contructed. For some reason, the env variables are not being picked up. Opening a PR to discuss the same.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,18 +1,49 @@
 from enum import Enum
 
-from fastapi import FastAPI, UploadFile
+from functools import lru_cache
+from fastapi import Depends, FastAPI
+from typing_extensions import Annotated
+
+from . import config
 
 app = FastAPI(title="Recipe Buddy")
 
+# Keywords that are used in the APIs
+API_KEY_QUERY_KEYWORD = "?apiKey="
+RANDOM_RECIPE_QUERY_KEYWORD = "/random"
+
+
+@lru_cache()
+def get_settings():
+    return config.Settings()
 
 @app.get("/")
 async def home():
     return "Welcome to Recipe Buddy!"
 
+"""
+Use Spoonacular API to fetch one random recipe
+"""
+def get_spoonacular_random_recipe(settings):
+    randome_recipe_api = settings.spoonacular_base_url + RANDOM_RECIPE_QUERY_KEYWORD \
+        + API_KEY_QUERY_KEYWORD + settings.spoonacular_api_key
+    print(randome_recipe_api)
 
-@app.get("/recipes/surpriseme/")
-async def get_recipes_random():
+
+"""
+Use GPT-4 API to fetch one random recipe
+"""
+def get_gpt4_random_recipe():
     pass
+
+@app.get("/recipes/random")
+async def get_recipes_random(apiChoice: str, settings: Annotated[config.Settings, \
+    Depends(get_settings)]):
+    if settings.default_backend.upper() == apiChoice.upper():
+        print('Base URL: ', settings.spoonacular_base_url)
+        get_spoonacular_random_recipe(settings)
+    else:
+        get_gpt4_random_recipe()
 
 
 class Cuisine(str, Enum):
@@ -28,9 +59,9 @@ async def get_recipes_by_cuisine(cuisine: Cuisine):
     pass
 
 
-@app.get("/recipes/{ingredients}")
-async def get_recipes_by_ingredients(ingredients: list[str]):
-    pass
+# @app.get("/recipes/{ingredients}")
+# async def get_recipes_by_ingredients(ingredients: list[str]):
+#     pass
 
 
 @app.get("/recipes/{mealcourse}")
@@ -38,12 +69,12 @@ async def get_recipes_by_mealcourse(mealcourse: str):
     pass
 
 
-@app.get("/recipes/{image}")
-async def get_recipes_by_image(image: UploadFile):
-    pass
+# @app.get("/recipes/{image}")
+# async def get_recipes_by_image(image: UploadFile):
+#     pass
 
 
-@app.get("/recipes/{images}")
-async def get_recipe_by_images(images: UploadFile):
-    pass
+# @app.get("/recipes/{images}")
+# async def get_recipe_by_images(images: UploadFile):
+#     pass
 
