bf79cf4f0fa95b8fef602c7763793c8d5081e6e5
Sambhavi PD <sambhavi@golittlebig.com>
2023-04-07 12:43:44 +0530

    incorporated all review comments

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,6 +6,8 @@ from functools import lru_cache
 from fastapi import Depends, FastAPI, status, UploadFile
 from typing_extensions import Annotated
 
+from pydantic import BaseModel
+
 from . import config
 
 app = FastAPI(title="Recipe Buddy")
@@ -17,6 +19,16 @@ RECIPE_BY_CUISINE_QUERY_KEYWORD = "/complexSearch"
 @lru_cache()
 def get_settings():
     return config.Settings()
+
+"""
+Class that corresponds to requests.model.Response
+Adding only 3 fields now. As need arises we can add
+more like content, headers, etc.,
+"""
+class ResponseModel(BaseModel):
+    success: bool
+    message: str
+    data: dict
 
 
 @app.get("/")
@@ -58,15 +70,23 @@ class Cuisine(Enum):
     THAI = "Thai"
     VIETNAMESE = "Vietnamese"
 
+
 """
 Use Spoonacular API to fetch recipes by cuisine
 """
-def get_spoonacular_recipes_by_cuisine(settings, input_cuisine, number_of_recipes):
-    recipes_by_cuisine_api = settings.spoonacular_base_url + RECIPE_BY_CUISINE_QUERY_KEYWORD \
-        + API_KEY_QUERY_KEYWORD + settings.spoonacular_api_key + "&cuisine=" \
-        + input_cuisine + "&number=" + str(number_of_recipes)
-    response = requests.get(recipes_by_cuisine_api)
-    return response.json()
+def get_spoonacular_recipes_by_cuisine(base_url, api_key, input_cuisine, number_of_recipes):
+    recipes_by_cuisine_url = f"{base_url}{RECIPE_BY_CUISINE_QUERY_KEYWORD}?&cuisine=\
+        {input_cuisine}&number={str(number_of_recipes)}"
+    headers = {"X-API-KEY" : api_key}
+
+    response = requests.get(recipes_by_cuisine_url, headers=headers)
+    response_data = {
+        "response_data" : response.json()
+    }
+    final_response = ResponseModel(success=True, \
+                    message=f"Successfully returned recipes by {input_cuisine.capitalize()} Cuisine", \
+                    data=response_data)
+    return final_response
 
 
 def get_gpt4_recipes_by_cuisine():
@@ -75,19 +95,25 @@ def get_gpt4_recipes_by_cuisine():
 """
 API to fetch recipes by cuisine
 """
-@app.get("/recipes/cuisine")
-async def get_recipes_by_cuisine(apiChoice: str, settings: Annotated[config.Settings, \
+@app.get("/recipes/cuisine", status_code=200, response_model=ResponseModel)
+async def get_recipes_by_cuisine(api_choice: str, settings: Annotated[config.Settings, \
         Depends(get_settings)], input_cuisine: str, number_of_recipes: int = 1):
     cuisines = [cuisine.value for cuisine in Cuisine]
     if input_cuisine.capitalize() in cuisines:
-        if settings.default_backend.upper() == apiChoice.upper():
-            recipes = get_spoonacular_recipes_by_cuisine(settings, input_cuisine, \
-                number_of_recipes)
+        if settings.default_backend.upper() == api_choice.upper():
+            recipes = get_spoonacular_recipes_by_cuisine(base_url=settings.spoonacular_base_url, \
+                        api_key=settings.spoonacular_api_key, \
+                        input_cuisine=input_cuisine, number_of_recipes=number_of_recipes)
             return recipes
         else:
             recipes = get_gpt4_recipes_by_cuisine()
     else:
-        return status.HTTP_404_NOT_FOUND
+        error_response = ResponseModel(success=False, \
+                    message=f"Sorry, no recipes of {input_cuisine} were found",
+                    data={
+                        "error_code" : status.HTTP_404_NOT_FOUND
+                        })
+        return error_response
 
 
 # @app.get("/recipes/{ingredients}")
