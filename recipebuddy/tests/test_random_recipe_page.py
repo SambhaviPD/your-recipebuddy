1b21cc7a284e5d2eb6cfb8aed68bd469ce83cfeb
Sambhavi PD <sambhavi@golittlebig.com>
2023-05-17 10:45:19 +0530

    test cases for random recipe page (#60)
    * added test cases to verify the contents of random recipe page
    
    * deleted few repeating files
    
    * added .py extension to random recipe test
    
    * modified sevice name in urls
    
    * modified frontend service to localhost in urls
    
    * modified port number for backend
    
    * removed absolute path for configuration
    
    * changed path for uvicorn run

---
+++ recipebuddy/tests/test_random_recipe_page.py
@@ -0,0 +1,33 @@
+import requests
+import re
+
+from playwright.sync_api import Page, expect
+
+def test_page_title(page: Page):
+    page.goto("http://localhost:8501/Random_Recipe")
+
+    expect(page).to_have_title(re.compile("Random Recipe"))
+
+
+def test_page_static_content(page: Page):
+    page.goto("http://localhost:8501/Random_Recipe")
+
+    expected_string1 = "Why don't you let the app suggest something"
+    
+    expect(page.get_by_text(expected_string1)).to_be_visible()
+
+    expected_string2 = "Who does not love surprises?"
+
+    expect(page.get_by_text(expected_string2)).to_be_visible()
+
+
+def test_page_successful_response(page: Page):
+    page.goto("http://localhost:8501/Random_Recipe")
+
+    API_URL = "http://backend:8080/recipebuddy/random_recipe"
+
+    response = requests.get(API_URL)
+    page.wait_for_timeout(5000)
+
+    expected_response = "Successfully returned a random recipe. Enjoy!"
+    assert response.json()["message"] == expected_response
