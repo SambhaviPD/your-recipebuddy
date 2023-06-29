cb9610a0d66fc39e3b40165cc88df28e0609d761
Sambhavi PD <sambhavi@golittlebig.com>
2023-06-29 17:06:23 +0530

    removed the master list check as a temporary step

--- recipebuddy/backend/src/recipebuddy_backend/main.py
+++ recipebuddy/backend/src/recipebuddy_backend/main.py
@@ -210,13 +210,16 @@ Use Spoonacular API to fetch recipes by ingredients
 def get_spoonacular_recipes_by_ingredients(
     base_url, api_key, selected_ingredients, custom_ingredients, number_of_recipes
 ):
+    print(selected_ingredients)
+    selected_ingredients = ", ".join(selected_ingredients)
+    print(selected_ingredients)
     recipes_by_ingredient_url = f"{base_url}{RECIPE_BY_INGREDIENTS_QUERY_KEYWORD}?&ingredients=\
         {selected_ingredients},{custom_ingredients}&number={str(number_of_recipes)}"
     headers = {"X-API-KEY": api_key}
 
-    print("recipes_by_ingredient_url: ", recipes_by_ingredient_url)
     response = requests.get(recipes_by_ingredient_url, headers=headers)
     response_data = {"response_data": response.json()}
+    print(response_data)
     final_response = ResponseModel(
         success=True,
         message=f"Successfully returned {number_of_recipes} recipe(s) that uses \
@@ -248,35 +251,18 @@ async def get_recipes_by_ingredients(
     custom_ingredients: str = Query(None),
     number_of_recipes: int = 1,
 ):
-    # Fetching enum values to a list
-    ingredients = [ingredient.value for ingredient in Ingredient]
-    # Split the string so that we will be able to compare individual ingredient
-    selected_ingredients_list = [
-        ingredient for ingredient in selected_ingredients.split(",")
-    ]
-    # Difference between input list and master list
-    additional_ingredients = set(selected_ingredients_list).difference(set(ingredients))
-    # If we do not have any additional ingredients, we proceed
-    if not additional_ingredients:
-        if settings.default_backend.upper() == api_choice.upper():
-            recipes = get_spoonacular_recipes_by_ingredients(
-                base_url=settings.spoonacular_base_url,
-                api_key=settings.spoonacular_api_key,
-                selected_ingredients=selected_ingredients,
-                custom_ingredients=custom_ingredients,
-                number_of_recipes=number_of_recipes,
-            )
-            return recipes
-        else:
-            recipes = get_gpt4_recipes_by_ingredients()
-    # Throw an error if any additional ingredient is present as part os user input
-    else:
-        error_response = ResponseModel(
-            success=False,
-            message=f"Sorry, {additional_ingredients} are not present in our master list. ",
-            data={"error_code": status.HTTP_404_NOT_FOUND},
+
+    if settings.default_backend.upper() == api_choice.upper():
+        recipes = get_spoonacular_recipes_by_ingredients(
+            base_url=settings.spoonacular_base_url,
+            api_key=settings.spoonacular_api_key,
+            selected_ingredients=selected_ingredients,
+            custom_ingredients=custom_ingredients,
+            number_of_recipes=number_of_recipes,
         )
-        return error_response
+        return recipes
+    else:
+        recipes = get_gpt4_recipes_by_ingredients()
 
 
 # Valid Meal courses that an end user is allowed to send
